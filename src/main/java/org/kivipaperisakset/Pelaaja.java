fd2af814d219f9dfdfc1230ae3068b2db5831454
Matleena Kankaanpää <104016919+mkankaan@users.noreply.github.com>
2024-04-29 23:51:12 +0300

    Pelaaja-javadoc

--- src/main/java/org/kivipaperisakset/Pelaaja.java
+++ src/main/java/org/kivipaperisakset/Pelaaja.java
@@ -4,40 +4,56 @@ package org.kivipaperisakset;
 import org.kivipaperisakset.valinta.Valinta;
 
 /**
+ * Pelaaja-luokka kuvaa pelin pelaajaa.
  *
- * @author Ira Dook
+ * @author Ira Dook, Matleena Kankaanpää
+ * @version 1.0 (2023-04-29)
  */
 
 public class Pelaaja {
     private static int juoksevaNumero = 1;
     private int pelaajaNumero, voitot;
 
+    /**
+     * Konstruktori.
+     */
     public Pelaaja() {
+        final int MAX_PELAAJIEN_LKM = 2;
         this.pelaajaNumero = juoksevaNumero++;
-        final int PELAAJIEN_MAX_LKM = 2;
 
-        if (juoksevaNumero > PELAAJIEN_MAX_LKM) {
+        if (juoksevaNumero > MAX_PELAAJIEN_LKM) {
             resetoiPelaajaNumero();
         }
     }
 
+    /**
+     * Palauttaa pelaajalle kertyneet voitot kokonaislukuna.
+     * @return Pelaajan voitot kokonaislukuna.
+     */
     public int getVoitot() {
         return voitot;
     }
 
+    /**
+     * Kasvattaa pelaajan voittojen määrää yhdellä.
+     */
     public void lisaaVoitto() {
         voitot++;
     }
 
+    /**
+     * Palauttaa satunnaisesti yhden Valinta-enumin arvoista.
+     * @return Satunnaisesti arvottu Valinta-enumin arvo.
+     */
     public Valinta valitse() {
         Valinta[] vaihtoehdot = Valinta.values();
         return vaihtoehdot[arvoSatunnaisluku(vaihtoehdot.length)];
     }
 
-    private int arvoSatunnaisluku(int ylaraja) {
-        return (int) (Math.random() * ylaraja);
-    }
-
+    /**
+     * Tulostaa ja palauttaa tiedon pelaajalle kertyneistä voitoista merkkijonona.
+     * @return Merkkijono, joka ilmaisee pelaajan voittojen määrän.
+     */
     public String tulostaVoitot() {
         String muoto = pelaajaNumero == 1 ? "ä" : "a";
         String monikko = voitot == 1 ? "" : "a";
@@ -46,11 +62,23 @@ public class Pelaaja {
         return viesti;
     }
 
+    /**
+     * Resetoi pelaajanumerona toimivan juoksevan luokkamuuttujan
+     * takaisin arvoon 1.
+     */
+    public static void resetoiPelaajaNumero() {
+        juoksevaNumero = 1;
+    }
+
+    /**
+     * Tulostaa pelaajanumeron muodossa "Pelaaja {pelaajanumero}"
+     * @return Merkkijono, joka kertoo pelaajan numeron
+     */
     public String toString() {
         return "Pelaaja " + pelaajaNumero;
     }
 
-    public static void resetoiPelaajaNumero() {
-        juoksevaNumero = 1;
+    private int arvoSatunnaisluku(int ylaraja) {
+        return (int) (Math.random() * ylaraja);
     }
 }
